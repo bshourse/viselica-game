5f81d852e10c4c073658db141b4c73d2ca353911
Dmitry <agapov.box@gmail.com>
2020-12-17 00:01:03 +0300

    print viselica

--- result_printer.rb
+++ result_printer.rb
@@ -6,6 +6,8 @@ class ResultPrinter
     puts "Слово: #{get_word_for_print(game.letters, game.good_letters)}"
     puts "Ошибки: #{game.bad_letters.join(", ")}"
 
+    print_viselica(game.errors)
+
     if game.status == -1
       puts "Вы проиграли! :-("
       puts "Загаданное слово было: " + game.letters.join("")
@@ -33,5 +35,127 @@ class ResultPrinter
 
       return result
 
+  end
+
+  def print_viselica(errors)
+      case errors
+      when 0
+        #рисуем виселицу
+        puts '
+          _______
+          |/
+          |
+          |
+          |
+          |
+          |
+          |
+          |
+        __|________
+        |         |
+        '
+      when 1
+        # рисуем голову
+        puts '
+        _______
+        |/
+        |     ( )
+        |
+        |
+        |
+        |
+        |
+        |
+      __|________
+      |         |
+      '
+      when 2
+        #шею и т.д
+        puts '
+        _______
+        |/
+        |     ( )
+        |      |
+        |
+        |
+        |
+        |
+        |
+      __|________
+      |         |
+      '
+      when 3
+        puts '
+        _______
+        |/
+        |     ( )
+        |      |_
+        |        \\
+        |
+        |
+        |
+        |
+      __|________
+      |         |
+      '
+      when 4
+        puts '
+        _______
+        |/
+        |     ( )
+        |     _|_
+        |    /   \\
+        |
+        |
+        |
+        |
+      __|________
+      |         |
+      '
+      when 5
+        puts '
+        _______
+        |/
+        |     ( )
+        |     _|_
+        |    / | \\
+        |      |
+        |
+        |
+        |
+      __|________
+      |         |
+      '
+
+      when 6
+        puts '
+        _______
+        |/
+        |     ( )
+        |     _|_
+        |    / | \\
+        |      |
+        |     / \\
+        |    /   \\
+        |
+      __|________
+      |         |
+      '
+      when 7
+        puts '
+        _______
+        |/     |
+        |     (_)
+        |     _|_
+        |    / | \\
+        |      |
+        |     / \\
+        |    /   \\
+        |
+      __|________
+      |         |
+      '
+
+    end
   end
 end
